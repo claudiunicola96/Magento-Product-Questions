08d9e06ce96b959666738f6d4886a160b5ec32b2
claudiu <claudiunicola96@gmail.com>
2016-02-03 23:40:09 +0000

    Refactored code

--- app/code/local/Evozon/QA/Model/Qa.php
+++ app/code/local/Evozon/QA/Model/Qa.php
@@ -8,54 +8,60 @@
  */
 class Evozon_QA_Model_Qa extends Mage_Core_Model_Abstract
 {
+    const PENDING = 'pending';
+
     protected function _construct()
     {
         $this->_init('evozon_qa/qa');
     }
 
-
-    /*
-     * Validate and save the question in db.
+    /**
+     * Save the question in db.
      * Throw Exception if the post isn't right.
-     *
+     * @param $post
+     * @throws Exception
      */
     public function saveQaData($post)
     {
-        $postObject = new Varien_Object();
-        $postObject->setData($post)
-            ->setStatus('pending')
+        $this->setData($post)
+            ->setStatus(self::PENDING)
             ->setEnable(1)
             ->setCreateAt(date('Y-m-d H:i:s'));
-
-        $this->validateQaPost($postObject);
-
-//        $this->save();
-
+        $this->validateQaPost($this);
+        $this->save();
     }
 
-    /*
+    /**
      * Validate the post.
      * If a validation go wrong, throw Mage Exception
+     * @param $postObject
+     * @throws Mage_Core_Exception
      */
     private function validateQaPost($postObject)
     {
+        // Validate to exist product id in post
         if (empty($postObject->getProductId())) {
             Mage::throwException("Must exist a product id!");
         }
-        
 
+        // Validate product id, must exist in db.
         if (!in_array($postObject->getProductId(), Mage::getModel('catalog/product')->getCollection()->getAllIds())) {
             Mage::throwException("Must exist the product!");
         }
 
+        // Validate to exist customer id in post
         if (empty($postObject->getCustomerId())) {
             Mage::throwException("Must exist a customer id!");
         }
 
-        if (!in_array($postObject->getCustomerId(), Mage::getModel('customer/customer')->getCollection()->getAllIds())) {
+        // Validate customer id, must exist in db.
+        if (!in_array($postObject->getCustomerId(),
+            Mage::getModel('customer/customer')->getCollection()->getAllIds())
+        ) {
             Mage::throwException("Must exist the customer!");
         }
 
+        // Validate question field
         if (empty($postObject->getQuestion())) {
             Mage::throwException("Complete the question field.");
         }
--- app/code/local/Evozon/QA/controllers/IndexController.php
+++ app/code/local/Evozon/QA/controllers/IndexController.php
@@ -12,32 +12,27 @@ class Evozon_QA_IndexController extends Mage_Core_Controller_Front_Action
      * Add a question.
      * Conditions to add a question are: customer logged, question form not empty.
      * @return Mage_Core_Controller_Varien_Action
-     * @throws Exception
-     * @throws Zend_Validate_Exception
      */
     public function addQuestionAction()
     {
-
         $post = $this->getRequest()->getPost();
         if ($post && Mage::getSingleton('customer/session')->isLoggedIn()) {
             try {
-
                 Mage::getModel('evozon_qa/qa')->saveQaData($post);
-                Mage::getSingleton('catalog/session')->addSuccess('Question sent to administrator. We\'ll replay as soon posible.');
+                Mage::getSingleton('catalog/session')
+                    ->addSuccess('Question sent to administrator.We\'ll replay as soon posible.');
 
                 return $this->_redirectReferer();
-
             } catch (Mage_Core_Exception $e) {
                 Mage::getSingleton('catalog/session')->addNotice($e->getMessage());
 
                 return $this->_redirectReferer();
-
             } catch (Exception $e) {
-                Mage::logException($e);
-                Mage::getSingleton('catalog/session')->addError('Unable to submit your question. Please, try again later');
+                Mage::log('METHOD addQuestionAction ' + $e, null, 'questions.log');
+                Mage::getSingleton('catalog/session')
+                    ->addError('Unable to submit your question. Please, try again later');
 
                 return $this->_redirectReferer();
-
             }
         } else {
             return $this->_redirectReferer();
